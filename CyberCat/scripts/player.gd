102be62da2014a7518c847d8d8709ae574f121ac
Fernanda Tozzi <fernandatozzihonorio@gmail.com>
2025-11-30 22:04:47 -0300

    :sparkles: Criação Projeto CyberCat

---
+++ CyberCat/scripts/player.gd
@@ -0,0 +1,96 @@
+extends CharacterBody2D
+
+
+const SPEED = 100.0
+const JUMP_FORCE = -300.0
+
+var is_jumping:= false
+var is_hurted := false
+var knockback_vector := Vector2.ZERO
+var direction
+
+@onready var animation := $anim as AnimatedSprite2D
+@onready var remote_transform := $remote as RemoteTransform2D
+
+signal player_has_died()
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_FORCE
+		is_jumping = true
+	elif is_on_floor():
+		is_jumping = false
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	direction = Input.get_axis("ui_left", "ui_right")
+	
+	if direction:
+		velocity.x = direction * SPEED
+		animation.scale.x = direction
+	
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED )
+		
+		
+	if knockback_vector != Vector2.ZERO:
+		velocity = knockback_vector
+		
+	_set_state()
+	move_and_slide()
+
+func _on_hurtbox_body_entered(body: Node2D) -> void:
+	#if body.is_in_group("enemies"):
+		#queue_free()
+	if Globals.player_life < 0 :
+		queue_free()
+	else:
+		if $ray_right.is_colliding():
+			take_damage(Vector2(-200, -200))
+		
+		elif $ray_left.is_colliding():
+			take_damage(Vector2(200, -200))
+
+func follow_camera(camera):	
+	var camera_path = camera.get_path()
+	remote_transform.remote_path = camera_path
+	
+func take_damage(knockback_force := Vector2.ZERO, duration := 0.25):
+	
+	if Globals.player_life > 0:
+		Globals.player_life -= 1
+	else:
+		queue_free()
+		emit_signal("player_has_died")
+		
+	if knockback_force != Vector2.ZERO:
+		knockback_vector = knockback_force
+		
+		var knockback_tween := get_tree().create_tween()
+		knockback_tween.parallel().tween_property(self, "knockback_vector", Vector2.ZERO, duration)
+		animation.modulate = Color(1,0,0,1)
+		knockback_tween.parallel().tween_property(animation, "modulate", Color(1,1,1,1), duration)
+	
+	is_hurted = true
+	await get_tree().create_timer(.5).timeout
+	is_hurted = false
+
+func _set_state():
+	var state = "idle"
+	
+	if !is_on_floor():
+		state = "jump"
+	elif direction != 0:
+		state = "run"
+		
+	if is_hurted:
+		state = "hurt"
+		
+	if animation.name != state:
+		animation.play(state)
+
+
+	
---
+++ CyberCat/scripts/world_01.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+@onready var player := $player as CharacterBody2D
+@onready var camera := $camera as Camera2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	player.follow_camera(camera)
+	player.player_has_died.connect(reload_game)
+	Globals.coins = 0
+	Globals.player_life= 3
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func reload_game():
+	await get_tree().create_timer(1.0).timeout
+	get_tree().reload_current_scene()
